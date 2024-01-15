0cf91f37cb65b935a2349812903d2c02564233f0
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2024-01-16 00:40:59 +0900

    [Silver III] Title: N과 M (8), Time: 168 ms, Memory: 16208 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N;
+    private static int M;
+    private static int[] arr;
+    private static boolean[] visited;
+    private static int[] ans;
+    private static StringBuilder sb=new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+
+        arr=new int[N];
+        visited=new boolean[N];
+        ans=new int[M];
+
+        st=new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+    private static void dfs(int depth, int start){
+        if(depth == M){
+            for(int val:ans){
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=start;i<N;i++){
+            ans[depth]=arr[i];
+            dfs(depth+1, i);
+        }
+    }
+}
