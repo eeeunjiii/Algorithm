8a40fbdd8f98d5b280dcc6bb7c4d1bd2588b36d3
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2024-02-29 16:57:26 +0900

    [Gold II] Title: 저울, Time: 140 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int N=Integer.parseInt(br.readLine());
+        Integer[] weight=new Integer[N];
+
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            weight[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(weight);
+
+        int sum=0;
+
+        for(int i=0;i<N;i++) {
+            if(sum+1<weight[i]) break;
+            sum+=weight[i];
+//            System.out.println(sum);
+        }
+        System.out.println(sum+1);
+    }
+}
