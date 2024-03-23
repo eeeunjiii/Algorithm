cd567466a65f285a2d28ee5a11e2e5fc6c639de4
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2024-03-23 22:43:20 +0900

    [Gold V] Title: 동전 2, Time: 136 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,41 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int N;
-    private static int K;
-    private static int[] num;
-    private static int[] dp;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N=Integer.parseInt(st.nextToken());
-        K=Integer.parseInt(st.nextToken());
-
-        num=new int[N+1];
-        dp=new int[K+1];
-
-        for(int i=1;i<=K;i++) {
-            dp[i]=Integer.MAX_VALUE-1;
-        }
+        int N=Integer.parseInt(st.nextToken()); // 동전 종류의 개수
+        int K=Integer.parseInt(st.nextToken()); // 동전의 값
+        int[] coin=new int[N+1];
+        int[] dp=new int[K+1];
 
         for(int i=1;i<=N;i++) {
-            num[i]=Integer.parseInt(br.readLine());
+            coin[i]=Integer.parseInt(br.readLine());
         }
-        Arrays.sort(num); // 오름차순 정렬
 
-        for(int i=1;i<=N;i++) { // num
-            for(int j=num[i];j<=K;j++) { // num[i]~K (이렇게 하면 굳이 num이 j보다 작은 경우를 고려하지 않아도 됨. 이미 여기서 처리)
-                dp[j]=Math.min(dp[j], dp[j-num[i]]+1); // num을 더하기 이전의 인덱스를 가진 dp 값에 +1만 해주면 됨.
+        Arrays.sort(coin);
+
+        Arrays.fill(dp, Integer.MAX_VALUE-1);
+        dp[0]=0;
+
+        for(int i=1;i<=N;i++) {
+            for(int j=coin[i];j<=K;j++) {
+                dp[j]=Math.min(dp[j], dp[j-coin[i]]+1);
             }
         }
-        if(dp[K]==Integer.MAX_VALUE-1) dp[K]=-1;
 
+        if(dp[K]==Integer.MAX_VALUE-1) dp[K]=-1;
         System.out.println(dp[K]);
     }
 }
