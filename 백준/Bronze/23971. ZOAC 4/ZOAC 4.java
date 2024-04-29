522be9338eaabac62e31d2c858769e68800ef01e
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2024-04-30 01:01:07 +0900

    [Bronze III] Title: ZOAC 4, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -4,17 +4,17 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
 
-        double h = Double.parseDouble(st.nextToken());
-        double w = Double.parseDouble(st.nextToken());
-        double n = Double.parseDouble(st.nextToken());
-        double m = Double.parseDouble(st.nextToken());
+        double H=Double.parseDouble(st.nextToken());
+        double W=Double.parseDouble(st.nextToken());
+        double N=Double.parseDouble(st.nextToken());
+        double M=Double.parseDouble(st.nextToken());
 
-        int x = (int) Math.ceil(h/(n+1));
-        int y = (int) Math.ceil(w/(m+1));
+        int height=(int) Math.ceil(H/(N+1));
+        int width=(int) Math.ceil(W/(M+1));
 
-        System.out.println(x*y);
+        System.out.println(height*width);
     }
 }
