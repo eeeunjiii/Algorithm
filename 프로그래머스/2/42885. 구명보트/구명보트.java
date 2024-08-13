40ff0a9e1242b55eeadeba72c605e378a7d07430
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2024-08-13 14:23:22 +0900

    [level 2] Title: 구명보트, Time: 10.78 ms, Memory: 56.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int idx=0;
+        
+        Arrays.sort(people); // 50,50,70,80
+        
+        for(int i=people.length-1;i>=idx;i--) {
+            if(people[i]+people[idx]<=limit) {
+                answer++;
+                idx++;
+            } else {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
