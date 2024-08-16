f610e5a4329ef71cff4152e72a561afea1688fe6
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2024-08-16 17:37:40 +0900

    [Silver III] Title: N과 M (5), Time: 284 ms, Memory: 29412 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,16 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-import static java.util.Collections.reverseOrder;
-
 public class Main {
 
-    private static int N;
-    private static int M;
+    private static int N, M;
     private static int[] arr;
+    private static int[] ans;
     private static boolean[] visited;
     private static StringBuilder sb=new StringBuilder();
-    private static int[] ans;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -19,12 +16,12 @@ public class Main {
         N=Integer.parseInt(st.nextToken());
         M=Integer.parseInt(st.nextToken());
 
+        ans=new int[M];
         arr=new int[N];
         visited=new boolean[N];
-        ans=new int[M];
 
         st=new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++){
+        for(int i=0;i<N;i++) {
             arr[i]=Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
@@ -32,17 +29,19 @@ public class Main {
         dfs(0);
         System.out.println(sb);
     }
-    private static void dfs(int depth){
-        if(depth == M){
-            for(int v:ans){
+
+    private static void dfs(int depth) {
+        if(depth==M) {
+            for(int v:ans) {
                 sb.append(v).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for(int i=0;i<N;i++){
-            if (!visited[i]) {
-                visited[i] = true;
+
+        for(int i=0;i<N;i++) {
+            if(!visited[i]) {
+                visited[i]=true;
                 ans[depth]=arr[i];
                 dfs(depth+1);
                 visited[i]=false;
