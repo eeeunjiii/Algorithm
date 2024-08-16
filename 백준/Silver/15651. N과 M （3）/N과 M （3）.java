a6ad8ae248cbcc799a4f7697ef8373b28e398bf9
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2024-08-16 17:33:23 +0900

    [Silver III] Title: N과 M (3), Time: 404 ms, Memory: 68528 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,14 +1,15 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    private static int N;
-    private static int M;
+
+    private static int N, M;
     private static int[] arr;
     private static StringBuilder sb=new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
 
         N=Integer.parseInt(st.nextToken());
         M=Integer.parseInt(st.nextToken());
@@ -16,18 +17,19 @@ public class Main {
         arr=new int[M];
 
         dfs(0);
-
         System.out.println(sb);
     }
-    private static void dfs(int depth){
-        if(depth==M){
-            for(int i:arr){
-                sb.append(i).append(" ");
+
+    private static void dfs(int depth) {
+        if(depth==M) {
+            for(int v:arr) {
+                sb.append(v).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for(int i=0;i<N;i++){
+
+        for(int i=0;i<N;i++) {
             arr[depth]=i+1;
             dfs(depth+1);
         }
