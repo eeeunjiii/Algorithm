721c568aec93c5255f9f89f119ed110a93a063fb
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2025-06-18 10:52:34 +0900

    [Silver III] Title: 가희와 키워드, Time: 2300 ms, Memory: 225092 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -6,26 +6,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
+        StringBuilder sb=new StringBuilder();
 
         int N=Integer.parseInt(st.nextToken()); // 메모장에 적은 키워드 개수
         int M=Integer.parseInt(st.nextToken()); // 블로그에 쓴 글의 개수
 
-        Map<String, Integer> map=new HashMap<>();
+        Set<String> memo=new HashSet<>();
         for(int i=0;i<N;i++) {
-            map.put(br.readLine(), 1);
+            memo.add(br.readLine());
         }
 
         for(int i=0;i<M;i++) {
             st=new StringTokenizer(br.readLine(), ",");
 
-            while (st.hasMoreElements()) {
+            while(st.hasMoreElements()) {
                 String word=st.nextToken();
-                if(map.containsKey(word) && map.get(word)==1) {
-                    N--;
-                    map.put(word, 0);
-                }
+                memo.remove(word);
             }
-            System.out.println(N);
+            System.out.println(memo.size());
         }
     }
 }
