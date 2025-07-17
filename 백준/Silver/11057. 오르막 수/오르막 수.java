ec2124f3a4192c66c82662def78222592e6de291
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2025-07-17 09:53:03 +0900

    [Silver I] Title: 오르막 수, Time: 112 ms, Memory: 15184 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -2,22 +2,21 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int N;
-    private static int[][] dp; // 오르막 수의 길이, 오르막 수의 마지막 수
 
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
-        N=Integer.parseInt(br.readLine());
-        dp=new int[N+1][10];
+        int N=Integer.parseInt(br.readLine());
+        long[][] dp=new long[10001][10];
+        // 길이가 N인 오르막 수의 개수를 10,007로 나눈 나머지 출력
 
-        for(int i=0;i<10;i++){
+        for(int i=0;i<=9;i++) {
             dp[1][i]=1;
         }
 
-        for(int i=2;i<=N;i++){
-            for(int j=0;j<10;j++){
-                for(int l=0;l<=j;l++){
+        for(int i=2;i<=N;i++) {
+            for(int j=0;j<=9;j++) {
+                for(int l=0;l<=j;l++) {
                     dp[i][j]+=dp[i-1][l];
                     dp[i][j]%=10007;
                 }
@@ -25,10 +24,9 @@ public class Main {
         }
 
         long ans=0;
-        for(int i=0;i<10;i++){
+        for(int i=0;i<=9;i++) {
             ans+=dp[N][i];
         }
-
         System.out.println(ans%10007);
     }
 }
