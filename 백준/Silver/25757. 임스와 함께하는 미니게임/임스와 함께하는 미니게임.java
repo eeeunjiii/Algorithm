0d6dd9fe3e3325ccd0a6e39ab25a7a20c900d3d7
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2025-11-10 21:25:09 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 236 ms, Memory: 27440 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.java
@@ -8,48 +8,25 @@ public class Main {
         StringTokenizer st=new StringTokenizer(br.readLine());
 
         int N=Integer.parseInt(st.nextToken());
-        Character K=st.nextToken().charAt(0); // Y: 윷놀이(임스 같이 한 명), F: 같은 그림 찾기(2명), O: 원카드(3명)
+        int K=getGameType(st.nextToken().charAt(0)); // Y: 윷놀이(임스 같이 한 명), F: 같은 그림 찾기(2명), O: 원카드(3명)
 
-        String[] names=new String[N];
-        for(int i=0;i<N;i++) {
-            names[i]=br.readLine();
-        }
-
-        int cnt=0;
         Set<String> set=new HashSet<>();
-
-        if (K=='Y') {
-            for(int i=0;i<N;i++) {
-                if(!set.contains(names[i])) {
-                    cnt++;
-                    set.add(names[i]);
-                }
-            }
-        } else if (K=='F') {
-            int flag=0;
-            for(int i=0;i<N;i++) {
-                if(!set.contains(names[i])) {
-                    set.add(names[i]);
-
-                    if(flag==1) {
-                        cnt++;
-                        flag=0;
-                    } else flag++;
-                }
-            }
-        } else {
-            int flag=0;
-            for(int i=0;i<N;i++) {
-                if(!set.contains(names[i])) {
-                    set.add(names[i]);
-
-                    if(flag==2) {
-                        cnt++;
-                        flag=0;
-                    } else flag++;
-                }
-            }
+        int cnt=0;
+        
+        while (N-->0) {
+            String name=br.readLine();
+            cnt+=set.contains(name) ? 0:1;
+            set.add(name);
         }
-        System.out.println(cnt);
+        System.out.println(cnt/K);
+    }
+    
+    private static int getGameType(char c) {
+        switch (c) {
+            case 'Y': return 1;
+            case 'F': return 2;
+            case 'O': return 3;
+        }
+        return -1;
     }
 }
