10470b074a8aa1d3d432b2ee40aa8c52a5dc64be
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2026-01-02 13:01:05 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 128 ms, Memory: 14720 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -11,27 +11,28 @@ public class Main {
         int[] arr=new int[N+1];
         int[] dp=new int[N+1];
 
-        int answer=0;
-
         StringTokenizer st=new StringTokenizer(br.readLine());
         for(int i=1;i<=N;i++) {
             arr[i]=Integer.parseInt(st.nextToken());
+        }
 
+        // 가장 긴 증가하는 부분 수열 구하기
+        int ans=0;
+        for(int i=1;i<=N;i++) {
             for(int j=0;j<i;j++) {
                 if(arr[i]>arr[j]) {
-                    dp[i]=Math.max(dp[i], dp[j] + 1);
-                    answer=Math.max(answer, dp[i]);
+                    dp[i]=Math.max(dp[i], dp[j]+1);
+                    ans=Math.max(ans, dp[i]);
                 }
             }
         }
-
-        sb.append(answer).append("\n");
+        sb.append(ans).append("\n");
 
         Stack<Integer> stack=new Stack<>();
         for(int i=N;i>=1;i--) {
-            if(dp[i]==answer) {
+            if(dp[i]==ans) {
                 stack.push(arr[i]);
-                answer--;
+                ans--;
             }
         }
         while(!stack.isEmpty()) {
