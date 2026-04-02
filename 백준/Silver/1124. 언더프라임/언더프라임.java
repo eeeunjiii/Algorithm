0d2b9d0c8d1d3bb31a1c9ff14b59b2cd52b31660
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2026-04-02 23:44:24 +0900

    [Silver I] Title: 언더프라임, Time: 148 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.java
+++ 백준/Silver/1124. 언더프라임/언더프라임.java
@@ -9,52 +9,34 @@ public class Main {
 
         int A=Integer.parseInt(st.nextToken());
         int B=Integer.parseInt(st.nextToken());
-        int[] dp=new int[B+1];
-
-        dp[2]=1; // 소수로만 이루어진 인수 개수
-        dp[3]=1;
-
-        for (int i=4;i<=B;i++) {
-            if (isPrime(i)) { // 소수인 경우
-                dp[i]=1;
-            } else {
-                for (int j=2;j<i;j++) {
-                    if (i%j==0) {
-                        dp[i]=dp[j]+dp[i/j];
-                        break;
-                    }
-                }
-            }
-        }
-
         int cnt=0;
+        
         for (int i=A;i<=B;i++) {
-            if (isPrime(dp[i])) {
+            if (underPrime(i)) {
                 cnt++;
             }
         }
         System.out.println(cnt);
-
-        // 자연수 X를 소인수분해하면, 곱해서 X가 되는 소수의 목록
-        // 어떤 수 X를 소인분해 해서 구한 소수의 목록의 길이가 소수이면 -> 언더프라임
-        // A와 B 사이의 정수 중에서 언더프라임인 것의 개수
-
     }
-
-    static boolean isPrime(int num) {
-        if (num<2) {
-            return false;
+    
+    static boolean underPrime(int num) {
+        int cnt=0;
+        
+        for (int i=2;i*i<=num;i++) {
+            while (num%i==0) {
+                cnt++;
+                num/=i;
+            }
         }
-
-        if (num==2) {
-            return true;
+        if (num>1) {
+            cnt++;
         }
-
-        for (int i=2;i<num;i++) {
-            if (num%i==0) {
+        
+        for (int i=2;i*i<=cnt;i++) { // 소수 판별을 두 번 해야 함.
+            if (cnt%i==0) {
                 return false;
             }
         }
-        return true;
-    }
+        return cnt>1;
+    } 
 }
