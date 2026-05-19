fa1dfca12d65e3763704b111b69cae81a4d6fd1a
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2026-05-19 12:04:50 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 62.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int storey) {
+        int ans=0;
+        
+        while (storey>0) {
+            int num=storey%10;
+            storey/=10;
+            
+            if (num>5) {
+                ans+=10-num;
+                storey++;
+            } else if (num<5) {
+                ans+=num;
+            } else if (storey%10>=5) {
+                ans+=5;
+                storey++;
+            } else {
+                ans+=5;
+            }
+        }
+        return ans;
+    }
+}
