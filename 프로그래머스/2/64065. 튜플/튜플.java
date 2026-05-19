c34f61a5d4cbfe6772f1a5995ba6626ee6748680
eunjii <91798213+eeeunjiii@users.noreply.github.com>
2026-05-19 11:31:36 +0900

    [level 2] Title: 튜플, Time: 46.13 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String s) {
+        s=s.substring(2, s.length()-2).replace("},{", "-");
+        String[] arr=s.split("-");
+        
+        Arrays.sort(arr, Comparator.comparingInt(String::length));
+        
+        List<Integer> list=new ArrayList<>();
+        for (String str:arr) {
+            String[] temp=str.split(",");
+            
+            for (int i=0;i<temp.length;i++) {
+                int num=Integer.parseInt(temp[i]);
+                
+                if (!list.contains(num)) {
+                    list.add(num);
+                }
+            }
+        }
+        return list;
+    }
+}
